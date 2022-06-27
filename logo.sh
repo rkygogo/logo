9f0f8b629bec6b83dbb3a03222104b7f32460500
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-06-27 16:59:48 +0800

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -2,8 +2,8 @@ red='\033[0;31m'
 bblue='\033[0;34m'
 plain='\033[0m'
                           
-echo -e "${bblue} ░██     ░██      ░██ ██ ██                 ░█${plain}█   ░██     ░██   ░██     ░█${red}█   ░██${plain}  "
-echo -e "${bblue}  ░██   ░██      ░██             ░░█${plain}█       ░██  ░██      ░██  ░██${red}      ░██  ░██${plain}   "
+echo -e "${bblue} ░██     ░██      ░██ ██ ██         ░█${plain}█   ░██     ░██   ░██     ░█${red}█   ░██${plain}  "
+echo -e "${bblue}  ░██   ░██      ░██    ░░█${plain}█        ░██  ░██      ░██  ░██${red}      ░██  ░██${plain}   "
 echo -e "${bblue}   ░██ ░██      ░██ ${plain}                ░██ ██        ░██ █${red}█        ░██ ██  ${plain}   "
 echo -e "${bblue}     ░██        ░${plain}██    ░██ ██       ░██ ██        ░█${red}█ ██        ░██ ██  ${plain}  "
 echo -e "${bblue}     ░██ ${plain}        ░██    ░░██        ░██ ░██       ░${red}██ ░██       ░██ ░██ ${plain}  "
