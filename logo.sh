e3fead91c660bbd52a1c9865b0db6eb0a8da02aa
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-06-27 16:42:23 +0800

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -2,18 +2,11 @@ red='\033[0;31m'
 bblue='\033[0;34m'
 plain='\033[0m'
                           
-echo -e "${bblue} ░██   ░██     ░██   ░██     ░██${plain}   ░██    ░██     ░██      ░██ ██ ${red}██${plain} "
-echo -e "${bblue} ░██  ░██      ░██  ░██${plain}      ░██  ░██      ░██   ░██      ░██    ${red}░░██${plain} "            
-echo -e "${bblue} ░██ ██        ░██${plain} ██        ░██ ██         ░██ ░██      ░${red}██        ${plain} "
-echo -e "${bblue} ░██ ██       ${plain} ░██ ██        ░██ ██           ░██        ${red}░██    ░██ ██${plain} "
-echo -e "${bblue} ░██ ░${plain}██       ░██ ░██       ░██ ░██          ░${red}██         ░██    ░░██${plain}"
-echo -e "${bblue} ░${plain}██  ░░██     ░██  ░░██     ░██  ░░${red}██        ░██          ░██ ██ ██${plain} "
+echo -e "${bblue} ░██     ░██      ░██ ██ ██ ${plain}        ██   ░██     ░██   ░██     ░█${red}█   ░██${plain}  "
+echo -e "${bblue}  ░██   ░██      ░██  ${plain}   ░░██       ░██  ░██      ░██  ░██${red}      ░██  ░██${plain}   "
+echo -e "${bblue}   ░██ ░██      ░██ ${plain}                ░██ ██        ░██ █${red}█        ░██ ██  ${plain}   "
+echo -e "${bblue}     ░██        ░${plain} ██    ░██ ██      ░██ ██        ░█${red}█ ██        ░██ ██  ${plain}  "
+echo -e "${bblue}     ░██ ${plain}         ░██    ░░██       ░██ ░██       ░${red}██ ░██       ░██ ░██ ${plain}  "
+echo -e "${bblue}     ░█${plain} █          ░██ ██ ██        ░██  ░░${red}██     ░██  ░░██     ░██  ░░██ ${plain}  "
 
 
-
-echo -e "${bblue} ░██   ░██     ░██   ░██     ░██   ░██    ░██     ░██      ░██ ██ ██ "
-echo -e "${bblue} ░██  ░██      ░██  ░██      ░██  ░██      ░██   ░██      ░██    ░░██ "            
-echo -e "${bblue} ░██ ██        ░██ ██        ░██ ██         ░██ ░██      ░██         "
-echo -e "${bblue} ░██ ██        ░██ ██        ░██ ██           ░██        ░██    ░██ ██ "
-echo -e "${bblue} ░██ ░██       ░██ ░██       ░██ ░██          ░██         ░██    ░░██"
-echo -e "${bblue} ░██  ░░██     ░██  ░░██     ░██  ░░██        ░██          ░██ ██ ██ "
